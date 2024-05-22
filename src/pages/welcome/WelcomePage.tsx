f4df74ee60ec91fe59dc9fedb5460e5cadb89e9a
TheLuckyman30 <bnauta0@gmail.com>
2024-05-22 14:45:24 -0400

    Moved index.html into public folder

---
+++ src/pages/welcome/WelcomePage.tsx
@@ -0,0 +1,11 @@
+function WelcomePage () {
+    return (
+        <>
+            <div>
+                Welcome to the Premier Leauge
+            </div>
+        </>
+    )
+}
+
+export default WelcomePage;
