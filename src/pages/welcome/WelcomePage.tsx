bb5b684177bb20d17e00b5caf4329375015ecbf0
TheLuckyman30 <bnauta0@gmail.com>
2024-05-22 22:49:33 -0400

    Added custom font to Welcome Page

--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -2,7 +2,7 @@ import './WelcomePage.css';
 
 function WelcomePage() {
   return (
-    <div className="welcome-background">
+    <div className="welcome-background w-text">
       <div>Welcome to the Premier Leauge</div>
     </div>
   );
