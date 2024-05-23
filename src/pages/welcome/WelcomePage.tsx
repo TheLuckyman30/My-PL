38964e1647547b17b711d1aea116e3c464c608cc
TheLuckyman30 <bnauta0@gmail.com>
2024-05-22 23:07:29 -0400

    Added button on home page to go to Table page

--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@mui/material';
 import './WelcomePage.css';
 
 function WelcomePage() {
   return (
     <div className="welcome-background w-text">
       <div>Welcome to the Premier Leauge</div>
+      <Button variant="contained" href="/LeagueTable">
+        League Table
+      </Button>
     </div>
   );
 }
