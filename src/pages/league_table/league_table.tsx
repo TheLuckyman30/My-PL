720f49b69a5e35da090ab85aead5016fe066599e
TheLuckyman30 <bnauta0@gmail.com>
2024-07-26 12:41:06 -0400

    Added more sorting logic to teams

--- src/pages/league_table/league_table.tsx
+++ src/pages/league_table/league_table.tsx
@@ -11,16 +11,35 @@ interface Team {
   name: string;
   goalsFor: number;
   goalsAgainst: number;
+  goalDiff: number;
   points: number;
 }
 
 const intitalTeams: Team[] = [
-  { name: 'Totttenham Hotspur', goalsFor: 0, goalsAgainst: 0, points: 0 },
-  { name: 'Liverpool', goalsFor: 0, goalsAgainst: 0, points: 0 },
-  { name: 'Manchester United', goalsFor: 0, goalsAgainst: 0, points: 0 },
-  { name: 'Manchester City', goalsFor: 0, goalsAgainst: 0, points: 0 },
-  { name: 'Chelsea', goalsFor: 0, goalsAgainst: 0, points: 0 },
-  { name: 'Arsenal', goalsFor: 0, goalsAgainst: 0, points: 0 },
+  {
+    name: 'Totttenham Hotspur',
+    goalsFor: 0,
+    goalsAgainst: 0,
+    goalDiff: 0,
+    points: 0,
+  },
+  { name: 'Liverpool', goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+  {
+    name: 'Manchester United',
+    goalsFor: 0,
+    goalsAgainst: 0,
+    goalDiff: 0,
+    points: 0,
+  },
+  {
+    name: 'Manchester City',
+    goalsFor: 0,
+    goalsAgainst: 0,
+    goalDiff: 0,
+    points: 0,
+  },
+  { name: 'Chelsea', goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
+  { name: 'Arsenal', goalsFor: 0, goalsAgainst: 0, goalDiff: 0, points: 0 },
 ];
 
 function Team({ team }: { team: Team }) {
@@ -40,9 +59,7 @@ function Team({ team }: { team: Team }) {
         <div style={{ margin: '10px' }}>{team.name}</div>
         <div style={{ margin: '10px' }}>GF: {team.goalsFor}</div>
         <div style={{ margin: '10px' }}>GA: {team.goalsAgainst}</div>
-        <div style={{ margin: '10px' }}>
-          GD: {team.goalsFor - team.goalsAgainst}
-        </div>
+        <div style={{ margin: '10px' }}>GD: {team.goalDiff}</div>
         <div style={{ margin: '10px' }}>PTS: {team.points}</div>
       </div>
     </div>
@@ -50,7 +67,7 @@ function Team({ team }: { team: Team }) {
 }
 
 function LeagueTable() {
-  const [teams, setTeams] = useState<Team[]>(intitalTeams);
+  const [teams, setTeams] = useState<Team[]>(teamSort(intitalTeams));
   const [selectedTeam, setSelectedTeam] = useState<Team>(intitalTeams[0]);
 
   function changeSelectedTeam(event: SelectChangeEvent) {
@@ -68,7 +85,26 @@ function LeagueTable() {
         ? { ...team, points: team.points + change }
         : { ...team }
     );
-    setTeams(newTeams.sort((a, b) => b.points - a.points));
+    setTeams(teamSort(newTeams));
+  }
+
+  function teamSort(teams: Team[]): Team[] {
+    return teams.sort((a, b) => {
+      if (b.points !== a.points) {
+        return b.points - a.points;
+      }
+      if (b.goalDiff !== a.goalDiff) {
+        return b.goalDiff - a.goalDiff;
+      }
+      if (b.goalsFor !== a.goalsFor) {
+        return b.goalsFor - a.goalsFor;
+      }
+      if (b.goalsAgainst !== a.goalsAgainst) {
+        return b.goalsAgainst - a.goalsAgainst;
+      } else {
+        return a.name.localeCompare(b.name);
+      }
+    });
   }
 
   return (
