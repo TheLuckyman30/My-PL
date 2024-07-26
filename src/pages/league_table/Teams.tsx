1749dab992c36dc7ca10e5d91585d7cb31320630
TheLuckyman30 <bnauta0@gmail.com>
2024-07-26 16:10:54 -0400

    Moved Teams function to new tsx file

---
+++ src/pages/league_table/Teams.tsx
@@ -0,0 +1,27 @@
+import { Team } from './league_table_classes';
+
+function Teams({ team }: { team: Team }) {
+  return (
+    <div style={{ display: 'flex' }}>
+      <div
+        style={{
+          width: '50vw',
+          backgroundColor: 'tan',
+          borderRadius: '5px',
+          boxShadow: '5px 5px gray',
+          marginTop: '10px',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div style={{ margin: '10px' }}>{team.name}</div>
+        <div style={{ margin: '10px' }}>GF: {team.goalsFor}</div>
+        <div style={{ margin: '10px' }}>GA: {team.goalsAgainst}</div>
+        <div style={{ margin: '10px' }}>GD: {team.goalDiff}</div>
+        <div style={{ margin: '10px' }}>PTS: {team.points}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Teams;
--- src/pages/league_table/league_table.tsx
+++ src/pages/league_table/league_table.tsx
@@ -7,30 +7,7 @@ import {
 } from '@mui/material';
 import { useState } from 'react';
 import { intitalTeams, Team } from './league_table_classes';
-
-function Teams({ team }: { team: Team }) {
-  return (
-    <div style={{ display: 'flex' }}>
-      <div
-        style={{
-          width: '50vw',
-          backgroundColor: 'tan',
-          borderRadius: '5px',
-          boxShadow: '5px 5px gray',
-          marginTop: '10px',
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div style={{ margin: '10px' }}>{team.name}</div>
-        <div style={{ margin: '10px' }}>GF: {team.goalsFor}</div>
-        <div style={{ margin: '10px' }}>GA: {team.goalsAgainst}</div>
-        <div style={{ margin: '10px' }}>GD: {team.goalDiff}</div>
-        <div style={{ margin: '10px' }}>PTS: {team.points}</div>
-      </div>
-    </div>
-  );
-}
+import Teams from './Teams';
 
 function LeagueTable() {
   const [teams, setTeams] = useState<Team[]>(teamSort(intitalTeams));
