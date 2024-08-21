9c768695f40ba683219b1c57bd6d4ad3fa97eb94
TheLuckyman30 <bnauta0@gmail.com>
2024-08-20 22:34:02 -0400

    Added premier League font

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 import '../../css/pages/Game.css';
 import { Team } from '../../utils/classes/Team';
@@ -6,36 +5,35 @@ import {
   allMatches,
   allTeams,
   generateAll,
+  season,
 } from '../../utils/helpers/League_Generation';
 import { Match } from '../../utils/classes/Match';
-import MatchesList from './components/MatchesList';
 import LeagueTable from './components/League_Table';
 import NavBar from './components/NavBar';
+import { Date } from '../../utils/classes/Date';
 
 function Game() {
   const [teams, setTeams] = useState<Team[]>([]);
   const [matches, setMatches] = useState<Match[]>([]);
   const [openMatchList, setOpenMatchList] = useState<boolean>(false);
+  const [currentDate, setCurrentDate] = useState<Date | null>(null);
 
   useEffect(() => {
     generateAll();
     setTeams(allTeams);
     setMatches(allMatches);
+    setCurrentDate(season.startDate as Date | null);
   }, []);
 
   return (
     <div className="game-page">
-      <NavBar></NavBar>
+      <NavBar
+        currentDate={currentDate}
+        setCurrentDate={setCurrentDate}
+      ></NavBar>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <LeagueTable teams={teams}></LeagueTable>
       </div>
-      <Button onClick={() => setOpenMatchList(true)}>Open Match List</Button>
-      <MatchesList
-        matches={matches}
-        openMatchList={openMatchList}
-        setTeams={setTeams}
-        setOpenMatchList={setOpenMatchList}
-      ></MatchesList>
     </div>
   );
 }
--- src/pages/game/components/NavBar.tsx
+++ src/pages/game/components/NavBar.tsx
@@ -1,11 +1,39 @@
 import '../../../css/components/NavBar.css';
+import { Date } from '../../../utils/classes/Date';
 
-function NavBar() {
+interface NavBarProps {
+  currentDate: Date | null;
+  setCurrentDate: (newDate: Date) => void;
+}
+
+function NavBar({ currentDate, setCurrentDate }: NavBarProps) {
+  function nextDay() {
+    if (currentDate && currentDate.nextDate) {
+      setCurrentDate(currentDate.nextDate);
+    }
+  }
   return (
     <div className="navBar">
-      <a className="align-center" href="/">
+      <a className="anchor" href="/">
         <img className="image" src="src\assets\images\PL-Logo.png"></img>
       </a>
+      <div className="date">
+        <div>
+          Current Date:{' '}
+          {currentDate
+            ? currentDate.currentDayName +
+              ', ' +
+              currentDate.currentMonth +
+              ' ' +
+              currentDate.currentDay +
+              ', ' +
+              currentDate.currentYear
+            : 'Nothing'}
+        </div>
+        <div className="date-button" onClick={nextDay}>
+          Next Date
+        </div>
+      </div>
     </div>
   );
 }
