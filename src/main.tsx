a36df16b6cddbb01882bc2c2fb63874c4e73d580
TheLuckyman30 <bnauta0@gmail.com>
2024-09-10 22:39:27 -0400

    Changed to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import WelcomePage from './pages/welcome/WelcomePage';
 import Game from './pages/game/Game';
 
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <WelcomePage></WelcomePage>,
+  },
+  {
+    path: '/LeagueTable',
+    element: <Game></Game>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/My-PL/">
-      <Routes>
-        <Route path="/" element={<WelcomePage></WelcomePage>}></Route>
-        <Route path="/LeagueTable" element={<Game></Game>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
