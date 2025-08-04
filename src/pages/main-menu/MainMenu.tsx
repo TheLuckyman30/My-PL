29e2936b0f0129ec3dad4e9357a79ea09e939e5b
TheLuckyman30 <bnauta0@gmail.com>
2025-08-04 01:28:03 -0400

    Starting main menu text and functionality

--- src/pages/main-menu/MainMenu.tsx
+++ src/pages/main-menu/MainMenu.tsx
@@ -1,8 +1,15 @@
-import { useTeamStore } from '../../zustand/team-store';
+import '../../css/MainMenu.css';
 
 function MainMenu() {
-  const teams = useTeamStore((state) => state.teams);
-  return <div>{teams[1].name}</div>;
+  return (
+    <div className="main flex flex-col justify-center items-center">
+      <div className="text-6xl font-bold">World Football Simulator</div>
+      <div className="flex flex-row gap-15 text-4xl mt-20">
+        <div>New Save</div>
+        <div>Load Save</div>
+      </div>
+    </div>
+  );
 }
 
 export default MainMenu;
