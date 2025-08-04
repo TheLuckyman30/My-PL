429d0749b5e9d2cc64139e513dbdbf5454db19cb
TheLuckyman30 <bnauta0@gmail.com>
2025-08-04 11:35:57 -0400

    Implemented intial style for Main menu

--- src/pages/main-menu/MainMenu.tsx
+++ src/pages/main-menu/MainMenu.tsx
@@ -5,8 +5,13 @@ function MainMenu() {
     <div className="main flex flex-col justify-center items-center">
       <div className="text-6xl font-bold">World Football Simulator</div>
       <div className="flex flex-row gap-15 text-4xl mt-20">
-        <div>New Save</div>
-        <div>Load Save</div>
+        <div className="bg-sky-500 p-3 rounded-md text-white font-bold cursor-pointer">
+          New Game
+        </div>
+        <div className="bg-sky-500 p-3 rounded-md text-white font-bold cursor-pointer">
+          Load Game
+          {/*This is currently non functional until a save system is implemented*/}
+        </div>
       </div>
     </div>
   );
