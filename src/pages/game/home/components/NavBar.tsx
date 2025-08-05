5a5a00d039101436c43d2fb15693d912003d7764
TheLuckyman30 <bnauta0@gmail.com>
2025-08-05 14:59:02 -0400

    Added links and basic styles to navbar

--- src/pages/game/home/components/NavBar.tsx
+++ src/pages/game/home/components/NavBar.tsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom';
 
+const NAVBAR_OPTIONS = [
+  { name: 'Menu', link: '/' },
+  { name: 'Home', link: '/game/home' },
+];
+
 function NavBar() {
   return (
     <div className="flex justify-center">
-      <div className="flex fixed gap-5 p-3 border mt-5">
-        <Link to="/">Menu</Link>
-        <div>Home</div>
+      <div className="flex fixed gap-5 p-3 mt-5 items-center bg-sky-700 rounded-md shadow-md text-white font-bold">
+        {NAVBAR_OPTIONS.map((option) => (
+          <Link className="hover:-translate-y-0.5 duration-150" to={option.link}>
+            {option.name}
+          </Link>
+        ))}
+        <div>Janurary 1st, 2025</div>
       </div>
     </div>
   );
