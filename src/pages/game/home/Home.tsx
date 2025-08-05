c76e490166503bd966ed866ef24806c9abc24bf7
TheLuckyman30 <bnauta0@gmail.com>
2025-08-05 12:23:20 -0400

    Added player store to contain info relating to player

--- src/pages/game/home/Home.tsx
+++ src/pages/game/home/Home.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <div>This the Home Page</div>;
+  return (
+    <div className="grid grid-cols-2 h-lvh">
+      <div>
+        <Link to="/">Main Menu</Link>
+        <div>Date</div>
+        <div>Team Info</div>
+      </div>
+      <div>League Info</div>
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/zustand/player-store.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+import { Player } from "../utils/interfaces/Player"
+
+type PlayerStore = {
+    player: Player | null;
+    setPlayer: (newPlayer: Player | null) => void;
+}
+
+export const usePlayerStore = create<PlayerStore>((set) => ({
+    player: null,
+    setPlayer: (newPlayer: Player | null) => {
+        set({player: newPlayer})
+    }
+}));
