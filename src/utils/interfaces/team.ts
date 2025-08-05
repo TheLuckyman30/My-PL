188d74b73ed3ce48fd3db802a552b12885d50e13
TheLuckyman30 <bnauta0@gmail.com>
2025-08-05 16:20:47 -0400

    Expanded team interface

--- src/utils/interfaces/team.ts
+++ src/utils/interfaces/team.ts
@@ -1,3 +1,12 @@
 export interface Team {
     name: string
+    shortName: string;
+    threeLetterName: string;
+    goalsFor: number;
+    goalsAgainst: number;
+    goalDifference: number;
+    wins: number;
+    draws: number;
+    loses: number;
+    leaguePosition: number;
 }
