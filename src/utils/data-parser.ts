4f4be2b6b155a9ff2d8710f8c49d27e198fbee31
TheLuckyman30 <bnauta0@gmail.com>
2025-08-06 11:45:34 -0400

    Combined all of the data into one json file

--- src/utils/data-parser.ts
+++ src/utils/data-parser.ts
@@ -1,10 +1,4 @@
-import { League } from './interfaces/league';
-import { Team } from './interfaces/team';
-import { Player } from './interfaces/player';
-import leagues from '../data/temp-leagues.json';
-import teams from '../data/temp-teams.json';
-import players from '../data/temp-players.json';
-import { useTeamStore } from '../zustand/team-store';
+import data from '../data/temp-database.json';
 
 interface LeagueJSON {
   id: string;
@@ -24,48 +18,12 @@ interface PlayerJSON {
   teamId: string;
 }
 
+interface DataJSON {
+  leagues: LeagueJSON[];
+  teams: TeamJSON[];
+  players: PlayerJSON[];
+}
+
 export function parseJSONData() {
-  const setTeams = useTeamStore((state) => state.setTeams);
-
-  const allLeagues: LeagueJSON[] = leagues;
-  const allTeams: TeamJSON[] = teams;
-  const allPlayers: PlayerJSON[] = players;
-
-  const newLeagues: League[] = [];
-  const newTeams: Team[] = [];
-  const newPlayers: Player[] = [];
-
-  allLeagues.forEach((league) => {
-    newLeagues.push({ id: league.id, name: league.name, teams: [] });
-  });
-
-  allTeams.forEach((team) => {
-    const league = newLeagues.find((league) => league.id === team.leagueId);
-    if (league) {
-      newTeams.push({
-        id: team.id,
-        name: team.name,
-        shortName: team.shortName,
-        threeLetterName: team.threeLetterName,
-        goalsFor: 0,
-        goalsAgainst: 0,
-        goalDifference: 0,
-        wins: 0,
-        draws: 0,
-        loses: 0,
-        leaguePosition: 0,
-        league: league,
-        players: [],
-      });
-    }
-
-    allPlayers.forEach((player) => {
-      const team = newTeams.find((team) => team.id === player.teamId);
-      if (team) {
-        newPlayers.push({ name: player.name, team: team });
-      }
-    });
-  });
-
-  setTeams(newTeams);
+  const importedData: DataJSON = data;
 }
