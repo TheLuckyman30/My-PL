525ed28962ac7016414b323aa7e27cbcfe519f8b
TheLuckyman30 <bnauta0@gmail.com>
2025-08-21 17:11:32 -0400

    Updtated home page to only listen to players team

--- src/pages/game/home/Home.tsx
+++ src/pages/game/home/Home.tsx
@@ -1,26 +1,27 @@
 import { useUserStore } from '../../../zustand/user-store';
 import LeagueTable from '../../../components/LeagueTable';
 import { useTeamStore } from '../../../zustand/team-store';
+import { Link } from 'react-router-dom';
 
 function Home() {
   const user = useUserStore((state) => state.user);
-  const teams = useTeamStore((state) => state.teams);
+  const selectedTeam = useTeamStore((state) => state.teams.get(user.selectedTeamId));
 
-  if (user) {
-    const selectedTeam = teams.get(user.selectedTeamId);
-    if (selectedTeam) {
-      return (
-        <div className="grid grid-cols-2 h-full">
-          <div className="grid grid-rows-2">
-            <div className="text-center self-center text-4xl font-bold">[Team Info]</div>
-            <div className="text-center self-center text-4xl font-bold">
-              [Form/Upcoming Matches]
-            </div>
-          </div>
-          <LeagueTable leagueId={selectedTeam.leagueId} />
+  if (selectedTeam) {
+    return (
+      <div className="grid grid-cols-2 h-full">
+        <div className="grid grid-rows-2">
+          <Link
+            to={`/game/team/${selectedTeam.id}`}
+            className="text-center self-center text-4xl font-bold"
+          >
+            {selectedTeam.name}
+          </Link>
+          <div className="text-center self-center text-4xl font-bold">[Form/Upcoming Matches]</div>
         </div>
-      );
-    }
+        <LeagueTable leagueId={selectedTeam.leagueId} />
+      </div>
+    );
   }
 }
 
