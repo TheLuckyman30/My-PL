b593d778273e751e374a3ecc009aecb611a479d8
TheLuckyman30 <bnauta0@gmail.com>
2025-08-21 20:14:43 -0400

    Added more data to the league table

--- src/components/LeagueTable.tsx
+++ src/components/LeagueTable.tsx
@@ -5,17 +5,22 @@ interface LeagueTableProps {
   leagueId: string;
 }
 
+const LEAUGE_TABLE_HEADERS = ['Name', 'W', 'D', 'L', 'GD', 'PTS'];
+
 function LeagueTable({ leagueId }: LeagueTableProps) {
   const league = useLeagueStore((state) => state.leagues.get(leagueId));
 
   if (league) {
     return (
       <div className="flex justify-center items-center h-full w-full">
-        <div className="flex flex-wrap flex-col shadow-2xl p-8 border-2 border-zinc-500">
+        <table className="shadow-2xl border">
+          {LEAUGE_TABLE_HEADERS.map((header) => (
+            <th>{header}</th>
+          ))}
           {league.teamIds.map((teamId, index) => (
             <TableRow teamId={teamId} key={index} />
           ))}
-        </div>
+        </table>
       </div>
     );
   }
--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -9,7 +9,18 @@ function TableRow({ teamId }: TableRowProps) {
   const team = useTeamStore((state) => state.teams.get(teamId));
 
   if (team) {
-    return <Link to={`/game/team/${teamId}`}>{team.name}</Link>;
+    return (
+      <tr className="hover:scale-105 hover:shadow-2xl duration-150 bg-white">
+        <td className="p-5">
+          <Link to={`/game/team/${teamId}`}>{team.name}</Link>
+        </td>
+        <td className="p-5">{team.wins}</td>
+        <td className="p-5">{team.draws}</td>
+        <td className="p-5">{team.loses}</td>
+        <td className="p-5">{team.goalDifference}</td>
+        <td className="p-5">0</td>
+      </tr>
+    );
   }
 }
 
